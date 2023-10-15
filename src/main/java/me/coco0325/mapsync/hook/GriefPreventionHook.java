73d34e4cde121467be57979b10ae257a2d85e208
51403 <51403cat@gmail.com>
2023-10-15 22:34:21 +0800

    add mor permission types for griefprevention hook users can set the type they need in config.yml

--- src/main/java/me/coco0325/mapsync/hook/GriefPreventionHook.java
+++ src/main/java/me/coco0325/mapsync/hook/GriefPreventionHook.java
@@ -1,18 +1,28 @@
 package me.coco0325.mapsync.hook;
 
+import me.coco0325.mapsync.MapSync;
 import me.ryanhamshire.GriefPrevention.Claim;
+import me.ryanhamshire.GriefPrevention.ClaimPermission;
 import me.ryanhamshire.GriefPrevention.GriefPrevention;
 import org.bukkit.Location;
 import org.bukkit.entity.Player;
 
+import java.util.Objects;
+
 public class GriefPreventionHook {
+
+    private static final MapSync plugin = MapSync.instance;
 
     public boolean canCreateMap(Player player){
         Location l = player.getLocation().clone();
         l.setY(100);
         Claim claim = GriefPrevention.instance.dataStore.getClaimAt(l, true, null);
-        if(claim == null) return true;
-        return claim.allowGrantPermission(player) == null;
+        if(claim == null) {
+            return true;
+        }
+        if (Objects.equals(plugin.getConfig().getString("hooks.Permission_type"), "Admin")){
+            return claim.allowGrantPermission(player) == null;
+        }
+        return claim.hasExplicitPermission(player, ClaimPermission.valueOf(plugin.getConfig().getString("hooks.Permission_type")));
     }
-
 }
